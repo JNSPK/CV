69eef6eaf340d8420181c34b4b67909d30b7d45c
JNSPK <johan.nilsson.mail@gmail.com>
2022-07-31 18:43:05 +0200

    Intial Project - Itération #1

---
+++ Animations.js
@@ -0,0 +1,20 @@
+// HAMBURGER MIAAAAAM
+
+var NavBar = document.querySelector(".Navmenu");
+
+document.querySelector(".menubutton").addEventListener("click", function () {
+  NavBar.classList.toggle("Navmenuopen");
+});
+
+// ME
+
+// WORK
+
+// CONTACT
+
+// Dark Mode
+
+function darkmode() {
+  var corps = document.body;
+  corps.classList.toggle("dark-mode");
+}
